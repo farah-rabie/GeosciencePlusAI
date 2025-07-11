c04f97cfeab8ba0304992f3124d6ba235ff95aa2
Farah Rabie <147926533+farah-rabie@users.noreply.github.com>
2025-07-11 20:42:21 +0100

    Update ClusteringFunctions.py

--- Lib/ClusteringFunctions.py
+++ Lib/ClusteringFunctions.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
 import matplotlib.patches as mpatches
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.cluster import KMeans
